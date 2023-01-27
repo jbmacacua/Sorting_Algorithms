82e8e0e8978a6ac8ccc4fd433f21e7c9384bf157
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 16:16:54 +0800

    Create variable to check the left idx and right idx

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,4 +1,11 @@
 def merge_sort(elements):
     if len(elements) > 1:
-        left_array = elements[:len(elements)//2]
-        right_array = elements[len(elements)//2:]
+        left_elements = elements[:len(elements)//2]
+        right_elements = elements[len(elements)//2:]
+
+        merge_sort(left_elements)
+        merge_sort(right_elements)
+
+        left_idx = 0
+        right_idx = 0
+        merge_idx = 0
