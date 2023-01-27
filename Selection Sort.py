9cb28499b336a20e51518086f4fae13171a2621c
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 12:27:57 +0800

    add list, interchange the min value to index 0

---
+++ Selection Sort.py
@@ -0,0 +1,6 @@
+mylist = [97, 23, 100, 88, 66, 84, 7, 21, 52, 17]
+print("This is my list: ", mylist)
+
+min_value = min(mylist)
+min_index = mylist.index(min_value)
+mylist[0], mylist[min_index] = mylist[min_index], mylist[0]
