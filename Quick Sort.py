1b48fd307838db89a049a3123f3fb3d35050dda1
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 17:43:27 +0800

    call quicksort that are less than the pivot element

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,4 +1,9 @@
 def quicksort(numbers, left, right):
-
+    if left < right:
+        partition_pos = partition(numbers, left, right)
+        quicksort(numbers, left, partition_pos - 1)
+        quicksort(numbers, partition_pos + 1, right)
 
 def partition(numbers, left, right):
+
+
