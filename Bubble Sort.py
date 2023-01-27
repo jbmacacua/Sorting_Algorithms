0cc865f53d0c09c318d2556e8cd6f1d350b6a8f2
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 13:36:02 +0800

    if the element is larger then interchange

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,3 +1,10 @@
 mylist = [97, 23, 100, 88, 66, 84, 7, 21, 52, 17]
 print("This is my list: ", mylist)
 
+for elements in range(len(mylist)-1):
+    for element in range (len(mylist)-1):
+        if mylist[element] > mylist[element+1]:
+            mylist[element], mylist[element+1] = mylist[element+1], mylist[element]
+            print(mylist)
+        else:
+            print(mylist)
