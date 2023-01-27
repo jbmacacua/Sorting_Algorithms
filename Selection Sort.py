d1be75a2cb1e8e173aa8652fed704924a4e25a52
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 12:35:47 +0800

    Print the new list using selection sort

--- Selection Sort.py
+++ Selection Sort.py
@@ -3,6 +3,7 @@ print("This is my list: ", mylist)
 
 
 for element in range (len(mylist)):
-    min_value = min(mylist)
+    min_value = min(mylist[element:])
     min_index = mylist.index(min_value)
-    mylist[0], mylist[min_index] = mylist[min_index], mylist[0]
+    mylist[element], mylist[min_index] = mylist[min_index], mylist[element]
+print("New list after selection sort: ", mylist)
