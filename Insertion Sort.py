75b45e3af1d1b73e6c5da344653051e3a5705a8c
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 14:56:44 +0800

    make 2 variable that will store index and the value

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,10 +2,8 @@ def insertion_sort(mylist):
     for element in range(1, len(mylist)):
         val = mylist[element]
         ind = element - 1
-        while ind >= 0 and val < mylist[ind]:
-            mylist[ind + 1] = mylist[ind]
-            ind = ind - 1
-        mylist[ind + 1] = val
-        print(mylist)
+
+
+
 
 
