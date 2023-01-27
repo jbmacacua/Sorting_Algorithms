7d398ccdbf6eeb58e691ef817202bdc6efbbf07a
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-01-27 16:27:27 +0800

    Transfer left index of the right array into merge array

--- Merge Sort.py
+++ Merge Sort.py
@@ -9,3 +9,12 @@ def merge_sort(elements):
         left_idx = 0
         right_idx = 0
         merge_idx = 0
+
+        while left_idx < len(left_elements) and right_idx < len(right_elements):
+            if left_elements[left_idx] < right_elements[right_idx]:
+                elements[merge_idx] = left_elements[left_idx]
+                left_idx += 1
+            else:
+                elements[merge_idx] = right_elements[right_idx]
+                right_idx += 1
+            merge_idx += 1
